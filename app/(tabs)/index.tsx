b6b56a18158d44290fc85a6755a6f39a4dd18ac2
gszp1 <grzybekmateusz@poczta.fm>
2024-06-06 19:22:02 +0200

    Adds logo and acronym.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image} from 'react-native';
 
 export default function HomePage() { // keep file named index. App breaks down after renaming
   return (
     <View style={styles.view}>
+        <Image
+          source = {require('../../assets/images/AppLogo.png')}
+          style = {styles.image}
+        />
+        <Text style={styles.acronym}>SDDiP</Text>
     </View>
   );
 }
@@ -12,5 +17,17 @@ const styles = StyleSheet.create({
   view: {
       flex: 1,
       backgroundColor: "#21233D",
+      alignItems: 'center'
+  },
+  image: {
+    marginTop: 50,
+    width: 150,
+    height: 150,
+    borderRadius: 50
+  },
+  acronym: {
+    color: 'white',
+    fontSize: 40,
+    fontWeight: 'bold'
   }
 });
