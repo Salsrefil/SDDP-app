62aeb63906e08c77c674de542a237e118e9792c8
gszp1 <grzybekmateusz@poczta.fm>
2024-06-12 20:02:24 +0200

    Adds displaying ptp infromations.

--- app/(tabs)/PtpPage.tsx
+++ app/(tabs)/PtpPage.tsx
@@ -2,20 +2,43 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet} from "react-native";
 import TimeDisplay from '@/components/phpPageComponents/TimeDisplay';
 import UpdateTimeButton from '@/components/phpPageComponents/UpdateTimeButton';
+import InformationDisplay from '@/components/phpPageComponents/InformationDisplay';
 
 export default function PtpPage() {
-    const [ptpInformation, setPtpInformation] = useState({
-        clockCount: 0,
-        currentMaster: 'None',
-        currentOffset: 0,
+    const [ptpInformation, setPtpInformation] = useState({ // currently hardcoded, this data should be loaded via http requests
+        clockCount: 1,
+        currentMaster: 'e0:d5:5e:83:cd:13',
+        currentOffset: -60455223566229,
         currentTime: '2024-06-12 15:09:26.878196',
-        foreignMaster: 'False',
+        foreignMaster: true,
         masterDescription: null
     });
 
     return (
         <View style={styles.view}>
-            <TimeDisplay time={ptpInformation.currentTime}/>
+            <InformationDisplay
+                name={"Role"}
+                value={ptpInformation.foreignMaster ? "Master" : "Slave"}
+            />
+            <InformationDisplay
+                name={"Master MAC"}
+                value={ptpInformation.currentMaster}
+            />
+            <InformationDisplay
+                name={"Master Description"}
+                value={ptpInformation.currentMaster}
+            />
+            <TimeDisplay
+                time={ptpInformation.currentTime}
+            />
+            <InformationDisplay
+                name={"Current Offset"}
+                value={ptpInformation.currentOffset.toString()}
+            />
+            <InformationDisplay
+                name={"Clock Count"}
+                value={ptpInformation.clockCount.toString()}
+            />
             <UpdateTimeButton />
         </View>
     );
---
+++ components/phpPageComponents/InformationDisplay.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {StyleSheet, View, Text} from "react-native";
+
+interface InformationDisplayProps {
+    name: string,
+    value: string
+};
+
+const InformationDisplay: React.FunctionComponent<InformationDisplayProps> = ({name, value}) =>{
+    return (
+        <View style={stylesheet.InformationDisplay}>
+            <Text style={stylesheet.name}>
+                {name}
+            </Text>
+            <Text style={stylesheet.value}>
+                {value}
+            </Text>
+        </View>
+    );
+};
+
+const stylesheet = StyleSheet.create({
+    InformationDisplay: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2B2E46',
+        width: '80%',
+        height: 80,
+        borderRadius: 40
+    },
+    name: {
+        textAlign: 'center',
+        color: 'white',
+        fontWeight: 'bold',
+        width: '100%',
+        height: 32,
+        fontSize: 24
+    },
+    value: {
+        textAlign: 'center',
+        color: 'white',
+        width: '100%',
+        height: 40,
+        fontSize: 22
+    }
+});
+
+export default InformationDisplay;
