f6d2fc7fcc6e58a8fce105b3a7053ad28871c224
studencik997 <235891@edu.p.lodz.pl>
2024-06-16 11:12:51 +0200

    Fix redirection logic in NotFoundScreen

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,18 +1,26 @@
-import { Link, Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { useRouter } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/');
+    }, 0);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
     <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
+        <ThemedText type="link">Redirecting to home screen...</ThemedText>
       </ThemedView>
     </>
   );
